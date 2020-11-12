4c03c921b6071dd2b220dd8b589dc8091d734e1d
Andrey Khomutov <homutovan@live.com>
2020-11-13 01:40:10 +0700

    for deploy

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,23 @@
 'use strict'
 
 let userForm = new UserForm();
+let formHandler = (method, errorBox) => (data) => method(
+    data, 
+    (response) => response.success ? location.reload() : errorBox(response.data),
+    );
 
-let formHandler = (method, errorBox) => data => method(data, response => response.success ? location.reload() : errorBox(response.data));
+userForm.loginFormCallback = formHandler(
+    ApiConnector.login, 
+    userForm.setLoginErrorMessage.bind(userForm)
+    );
 
-userForm.loginFormCallback = formHandler(ApiConnector.login, userForm.setLoginErrorMessage.bind(userForm));
-userForm.registerFormCallback = formHandler(ApiConnector.register, userForm.setRegisterErrorMessage.bind(userForm));
+userForm.registerFormCallback = formHandler(
+    ApiConnector.register, 
+    userForm.setRegisterErrorMessage.bind(userForm)
+    );
+
+ApiConnector.login(
+    { 
+        login: 'John@Lennon.ru', 
+        password: 'forever' }
+)
